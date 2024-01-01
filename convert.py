bd7c24427e0a24c8306888c891b108feaf329591
adarshu21 <adarshu.cs21@rvce.edu.in>
2024-01-01 21:43:39 +0530

    complete implementaion of project

--- convert.py
+++ convert.py
@@ -27,6 +27,46 @@ def extract_title_text(input_path, output_path):
     output_tree.write(output_path)
 
 if __name__ == "__main__":
-    input_path = "/home/adarsh/Desktop/NLP-ENGLISH-DICTIONARY/test.xml"
-    output_path = "ans.xml"
+    input_path = "/home/adarsh/Desktop/NLP-ENGLISH-DICTIONARY/combined.xml"
+    output_path = "ansx.xml"
     extract_title_text(input_path, output_path)
+
+
+
+# import os
+# import xml.etree.ElementTree as ET
+
+# # Function to read individual XML files and extract root element
+# def extract_root_element(xml_file):
+#     tree = ET.parse(xml_file)
+#     return tree.getroot()   
+
+# # Function to combine root elements into a single XML file
+# def combine_elements_to_xml(elements):
+#     root = ET.Element('xml')
+#     for element in elements:
+#         root.append(element)
+#     return ET.tostring(root, encoding='unicode')
+
+# # Directory containing the input XML files
+# xml_directory = '/home/adarsh/Desktop/NLP-ENGLISH-DICTIONARY/analyzers/afterlang/input/x/x_0'
+
+# # List all XML files in the directory
+# xml_files = [f for f in os.listdir(xml_directory) if f.endswith('.xml')]
+
+# # List to store all root elements
+# all_elements = []
+
+# # Read each XML file and extract root elements
+# for xml_file in xml_files:
+#     root_element = extract_root_element(os.path.join(xml_directory, xml_file))
+#     all_elements.append(root_element)
+
+# # Combine all root elements into a single XML file
+# combined_xml = combine_elements_to_xml(all_elements)
+
+# # Save the combined XML to a file
+# with open('combined.xml', 'w') as f:
+#     f.write(combined_xml)
+
+# print("XML files combined successfully. Output file: combined.xml")
